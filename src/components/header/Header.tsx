91ab6fe712c3de17ce810b786eb4c05936c00c78
lhjwork <jin.come.up.business@gmail.com>
2025-10-28 11:46:32 +0900

    dialog view is appiled

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,6 +4,7 @@ import React, { useState } from "react";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { Dialog, DialogClose, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
+import LoginModal from "@/app/(beforeLogin)/_component/LoginModal";
 
 const Header = () => {
   const [isLoginDialogOpen, setIsLoginDialogOpen] = useState(false);
@@ -36,18 +37,8 @@ const Header = () => {
                 <Button>Login</Button>
               </DialogTrigger>
 
-              <DialogContent className="sm:max-w-[425px]">
-                <DialogHeader>
-                  <DialogTitle>Edit profile</DialogTitle>
-                  <DialogDescription>Make changes to your profile here. Click save when you&apos;re done.</DialogDescription>
-                </DialogHeader>
-
-                <DialogFooter>
-                  <DialogClose asChild>
-                    <Button variant="outline">Cancel</Button>
-                  </DialogClose>
-                  <Button type="submit">Save changes</Button>
-                </DialogFooter>
+              <DialogContent>
+                <LoginModal />
               </DialogContent>
             </div>
           </div>
