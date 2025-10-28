1f4be07369d00cac65e0dc5b2bf8a1aea17329c8
lhjwork <jin.come.up.business@gmail.com>
2025-10-28 17:49:03 +0900

    magic ui install and MarqueeDemo is applied

--- src/app/(beforeLogin)/page.tsx
+++ src/app/(beforeLogin)/page.tsx
@@ -1,4 +1,5 @@
 import { CarouselPlugin } from "@/components/carousel/Carousel";
+import { MarqueeDemo } from "@/components/Marquee/Marquee";
 
 export default function Home() {
   return (
@@ -6,6 +7,9 @@ export default function Home() {
       <main>
         <div className="w-full justify-center flex p-8">
           <CarouselPlugin />
+        </div>
+        <div className="w-full justify-center flex px-48 mt-36">
+          <MarqueeDemo />
         </div>
       </main>
     </div>
